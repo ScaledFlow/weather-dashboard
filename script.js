8bbe7d85e7274eb371454fd29d4229b320568e32
John Leintz <johnleintz@scaledflow.com>
2020-05-05 15:23:14 -0500

    added local storage for cities searched

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
-var cities = ["Atlanta", "Minneapolis", "New York"];
+// var cities = ["Atlanta", "Minneapolis", "New York"];
+var cities = [];
 var citiesStored = [];
 var $citiesList = $("<ul>");
 var searchCount = 0;
 var itemCount = 0;
 
-init();
-//pushCity("Denver");
-setLocalStore();
+// commented out lines for testing purposes
+//setLocalStore();
 //getLocalStore();
 //clearLocalStore();
-consoleCity();
-// addCityToList();
 //removeLocalStorage();
-
 // clearCities();
 
-function init() {}
+init();
+
+function init() {
+  var citiesExist = localStorage.getItem("cities");
+  if (citiesExist != null) {
+    getLocalStore();
+    for (i = cities.length; i > 0; i--) {
+      buildCitiesSearch(cities[i - 1]);
+    }
+  }
+}
 
 function removeLocalStorage() {
   localStorage.removeItem("cities");
@@ -32,7 +39,23 @@ $(document).ready(function () {
   });
 });
 
-// create ul and add cities searched to sidebar UI
+// Populate city side bar with past searches when application launches
+function buildCitiesSearch(city) {
+  if (searchCount === 0 && cities.length != 0) {
+    $citiesList.addClass("list-group");
+    $("#city-list-section").append($citiesList);
+    searchCount++;
+  }
+
+  itemCount++;
+  var $cityListItem = $("<li class='search-cities-list'></li>");
+  $cityListItem.addClass("list-group-item");
+  $cityListItem.addClass("list-group-city-" + itemCount);
+  $cityListItem.text(city);
+  $(".list-group").prepend($cityListItem);
+}
+
+// create ul and add cities searched to sidebar UI after search button is clicked
 function addCityToList(city) {
   if (searchCount === 0) {
     $citiesList.addClass("list-group");
@@ -49,14 +72,22 @@ function addCityToList(city) {
 }
 
 function pushCity(city) {
-  cities.push(city);
-  consoleCity();
+  // cities.push(city);
+  cities.unshift(city);
 }
 
 function setLocalStore() {
   if (cities.length != 0) {
     localStorage.setItem("cities", JSON.stringify(cities));
   }
+}
+
+function getLocalStore() {
+  var citiesRetrieval = localStorage.getItem("cities");
+  var citiesRetrieved = JSON.parse(citiesRetrieval);
+  cities = citiesRetrieved;
+  console.log(cities);
+  //localStorage.setItem("dayPlan", JSON.stringify(dayPlan));
 }
 
 function consoleCity() {
